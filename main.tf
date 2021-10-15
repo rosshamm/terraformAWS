df67b19f59568fde07abe161bb43eb368bf161ba
rosshamm <ross_hammond@yahoo.com>
2021-10-15 16:16:52 +0100

    eu change

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ terraform {
 
 # Download any stable version in AWS provider of 2.36.0 or higher in 2.36 train
 provider "aws" {
-  region  = "us-east-1"
+  region  = "eu-east-1"
   version = "~> 2.36.0"
 }
 
